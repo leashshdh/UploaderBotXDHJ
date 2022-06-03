2b77ffc8c29e510fd5dd8fdf8b617fdae94dffd8
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-03 15:54:47 +0530

    Update cb_buttons.py

--- plugins/cb_buttons.py
+++ plugins/cb_buttons.py
@@ -36,7 +36,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 @pyrogram.Client.on_callback_query()
 async def button(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
-        await bot.send_messages(
+        await bot.edit_message_text(
             chat_id=update.chat.id,
             text="Buy The Subscriptions From @LegendBoy_XD To Get Access Of Advanced Features Of This Bot",
             reply_to_message_id=update.message_id,
