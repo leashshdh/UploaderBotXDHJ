727f50424962ba59b7d922bdd00f71909f42aaf5
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-03 16:40:25 +0530

    Update convert_to_file.py

--- plugins/convert_to_file.py
+++ plugins/convert_to_file.py
@@ -38,7 +38,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 @pyrogram.Client.on_message(pyrogram.filters.command(["c2f"]))
 async def convert_to_audio(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
-        await bot.send_messages(
+        await bot.send_message(
             chat_id=update.chat.id,
             text="Buy The Subscriptions From @LegendBoy_XD To Get Access Of Advanced Features Of This Bot",
             reply_to_message_id=update.message_id,
