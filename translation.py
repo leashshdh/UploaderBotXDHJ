fca139b33c67f4fa05d165090bc23b8401d4ed09
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-03 16:59:04 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,4 +1,160 @@
 class Translation(object):
+    INFO = f"""
+╭────[ **🔅File Share Bot🔅**]────⍟
+│
+├🔸👨‍💻 **Developer:** [LegendBoy](https://t.me/LegendBoy_XD)
+│
+├🔸📝 **Language:** [Python3](https://www.python.org)
+│
+├🔹📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
+│
+├🔹📡 **Hosted On:** [Heroku](https://heroku.com) 
+│
+├🔹👥 **Bot Support:** [Support](https://t.me/LegendBot_OP)
+│
+├🔸🔔 **Bot Updates:** [Update](https://t.me/LegendBot_AI)
+│
+╰──────[ 😎 ]───────────⍟
+"""
+
+    DOWNLOAD_START = "📥DownloadinG📥 \n\nWait⏳ untill it completed.\nPlease Wait 2-3 Min According to Size\n"
+
+    UPLOAD_START = "📤UploadinG📤 \nPlease Wait A Few Minute According to Size"
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "Uploaded Successfully"
+
+    FF_MPEG_RO_BOT_RE_SURRECT_ED = "Syntax: Screenshot - /trim HH:MM:SS (specific time).\n Trim - /trim (start time)HH:MM:SS HH:MM:SS(end time)"
+
+    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First Send /downloadmedia - Download any media so that it can be downloaded to my local. \nSend /storageinfo - know the Storage Info, that is currently downloaded.\nTo Clear Download Media - /clearmediadownload"
+
+    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\n\nSend /cleardownloadmedia - Delete this media, from my storage."
+
+    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media Cleared Successfully."
+
+    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
+
+    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A Saved Media Already Storage First Clear The Old Storage Media - /cleardownloadmedia ."
+
+    CANCEL_STR = "Process Cancelled Successfully"
+
+    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+
+    DOWNLOAD_FILE = "📥DownloadinG📥 File Wait A Few Minute"
+
+    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram File Media to convert Supporting Stream Media"
+
+    CUSTOM_CAPTION_UL_FILE = " "
+
+    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album Should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
+
+    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = "Send Me Two photo to combine It"
+
+    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+
+    SLOW_URL_DECED = "Bro that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
+
+    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
+
+    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} Seconds.\n\nUploaded in {} Seconds."
+
+    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get Screenshots"
+
+    GO_FILE_UPLOAD = "📤UploadinG📤 \n\n To  gofile.io \nPlease Wait 5-6 Min According to Size"
+
+    AFTER_GET_GOFILE_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n<b>File MD5 Checksum :</b> <code>{}</code>\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\n Valid untill 10 days of inactivity\nJoin : @LegendBot_AI"
+
+    REPLY_TO_DOC_GET_LINK = "Reply to a Telegram media(Photo, Video , Song) to get High Speed Direct Download Link"
+
+    FILE_NOT_FOUND = "Error, File not Found!!"
+
+    HELP_USER = """This Id Advanced Features Bot 
+**CMD** - /info 
+**INFO** - To Know About Developer & About This Bot
+
+**CMD** - /downloadmedia
+**INFO** - This Is Used To Download Media Then Trim Your Video
+**USE** - /downloadmedia <reply to video>
+
+**CMD** - /cleardownloadmedia 
+**INFO** - This Is Used To Clear Download Video then U From Bot Database 
+
+**CMD** - /storageinfo 
+**INFO** - This Is Used to check downloaded video in your database
+**USE** -
+
+**CMD** - /trim
+**INFO** - This is used to trim video from video & Take screenshot when you send specific time
+**USE** - /trim HH:MM:SS HH:MM:SS or /trim HH:MM:SS
+
+**CMD** - /c2a
+**INFO** - This Is Used to convert video to audio
+**USE** - /c2a <reply to video>
+
+**CMD** - /c2f
+**INFO** - This Is Used To Convert Streaming Video To File Video
+**USE** - /c2f <reply to video>
+
+**CMD** - /c2v
+**INFO** - This Is Used To Convert File Video To Streaming Support Video
+**USE** - /c2f <reply to file video>
+
+**CMD** - /setthumb
+**INFO** - This Is Used to Combine Two Photo
+**USE** - /setthumb <reply to photo> __NOTE__ - Must Contain 2 PIC
+
+__NOTE__ - Send Me Img It Automatically Set Into Custom Thumbnail For DRM Protected Video
+
+**CMD** - /delthumb
+**INFO** - To Delete The Custom Thumbnail For DRM Protected Video
+
+__NOTE__ - Send Me Sticker To Convert Into File Or Pic
+
+**CMD** - /sshot
+**INFO** - To Take Screenshot From Video
+**USE** - /sshot <reply to video>
+
+**CMD** - /getlink
+**INFO** - To Get Fast Server To Share & Watch Online
+**USE** - /getlink <reply to media>
+
+**CMD** - /help
+**INFO** - To See All Command 
+
+**CMD** - /rename 
+**INFO** - To Rename telegram media with custom Thumbnail 
+**USE** - /rename <file name > <reply to video >
+
+**CMD** - /server
+**INFO** - To know about server ram, speed , uptime
+
+__NOTE__ - To Download DRM Protected Video Like Zee5, Hotstar, Amazon, Netflix Like Video & YouTube Video """
+
+    UPGRADE_TEXT = "<b>30rs - One Month</b><b>10rs - 7 Days </b>/help for Details"
+
+    REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to /rename with custom thumbnail support"
+    
+    EXTRACT_ZIP_INTRO_ONE = "Send a compressed file first, Then reply /unzip command to the file."
+    
+    EXTRACT_ZIP_INTRO_THREE = "Analyzing received file. ⚠️ This might take some time. Please be patient. "
+    
+    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
+
+    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to @lakshyajeepw2023freeh"
+
+    EXTRACT_ZIP_STEP_TWO = "Select file_name to upload from the below options. \nYou can use /rename command after receiving file to rename it with custom thumbnail support."
+
+    RCHD_BOT_API_LIMIT = "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
+    
+    IFLONG_FILE_NAME = " Only 64 characters can be named . "
+
+    SET_CUSTOM_USERNAME_PASSWORD = "This Video Is Only For Username & Password By Syntax link username|password"
+ 
+    NO_VOID_FORMAT_FOUND = "ERROR... {}"
+ 
+    FORMAT_SELECTION = "Choose The Size: <a href='{}'>File size might be approximate</a>.\nS-Video - Support Streaming\nD-Video - File Format "
+
+    CURENT_PLAN_DETAILS = "Your Telegram Id - {}\nPlan Type - {}\n Expire - {}"
+
     START_TEXT = """Hello <b>{message.from_user.first_name}</b>,
 This is a Telegram Bot Which can do many functions.
 
@@ -6,109 +162,43 @@ This is a Telegram Bot Which can do many functions.
 
 JOIN : https://t.me/LegendBot_AI\n\nThanks To @LegendBoy_XD
 """
-    IFLONG_FILE_NAME = " Only 64 characters can be named . "
+
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
+
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>No preminum plans available in this bot </b>  /help for Details"
-    FORMAT_SELECTION = (
-        "Choose The Size: <a href='{}'>File size might be approximate</a>.Click On Down Button for select size.\n Warning After Select Size Dont Send Anything & Kindly Wait.It Will take time according to size"
-        ""
-    )
-    SET_CUSTOM_USERNAME_PASSWORD = """\n\n🔥 Check @PWALLLECTURE To Get All Your Lecture \nThanks To @LegendBoy_XD"""
+   
     NOYES_URL = "@robot URL detected. Please use https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
-    DOWNLOAD_FILE = " 📥DownloadinG📥 File "
-    UPLOAD_FILE = (
-        " 📤UploadinG📤 \n\n To  transfer.sh \nPlease Wait 5-6 Min According to Size"
-    )
-    ANNO_UPLOAD = (
-        " 📤UploadinG📤 \n\n To  anonfiles.com \nPlease Wait 5-6 Min According to Size"
-    )
-    BAY_UPLOAD = (
-        " 📤UploadinG📤 \n\n To  bayfiles.com \nPlease Wait 5-6 Min According to Size"
-    )
-    GO_FILE_UPLOAD = (
-        " 📤UploadinG📤 \n\n To  gofile.io \nPlease Wait 5-6 Min According to Size"
-    )
-    DOWNLOAD_START = " 📥DownloadinG📥 \n\nWait⏳ untill it completed.\nPlease Wait 5-6 Min According to Size"
-    UPLOAD_START = " 📤UploadinG📤 \nPlease Wait 1-2 Min According to Size"
-    RCHD_BOT_API_LIMIT = (
-        "size greater than maximum allowed size (50MB). Neverthless, trying to upload."
-    )
-    RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 2GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = (
-        "Suport : https://t.me/LegendBot_AI\n Thanks To @LegendBoy_XD ♥️"
-    )
-    AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nSupport : https://t.me/LegendBot_AI\nUploaded in {} seconds."
+
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/pw_upload_linkbot'>@LegendBot_AI</a>"
-    SAVED_CUSTOM_THUMB_NAIL = "Custom video / file thumbnail saved. This image will be used in the video / file."
+
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription</a>"
+
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
-    FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
-    SAVED_RECVD_DOC_FILE = "Document Downloaded Successfully."
-    CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
-    NO_VOID_FORMAT_FOUND = "ERROR... {}"
-    FILE_NOT_FOUND = "Error, File not Found!!"
+
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
+
     CURENT_PLAN_DETAILS = """About you...
 --------
 Telegram ID : <code>{}</code>
 """
-    HELP_USER = """Hii I am <b>bot</b> and I can perform many tasks.
 
-1.) Send url (Link|New Name with Extension)
-2.) Wait For Some Time.
-3.) Select the button.
-   SVideo - Give File as video(Use This Button To Get Fast Video)
-   DFile  - Give File with Screenshots
-   Video  - Give File as video without Screenshots
-   DFile  - Give File without Screenshots
+    AFTER_GET_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nJoin : @Lakshyajee2pw"
 
-   Support : https://t.me/LegendBot_AI \n\n\\Thanks To @LegendBoy_XD"""
-    REPLY_TO_DOC_GET_LINK = (
-        "Reply to a Telegram media to get High Speed Direct Download Link"
-    )
-    REPLY_TO_DOC_FOR_C2V = "Reply to a Telegram media to convert"
-    REPLY_TO_DOC_FOR_SCSS = "Reply to a Telegram media to get screenshots"
-    REPLY_TO_DOC_FOR_RENAME_FILE = (
-        "Reply to a Telegram media to /ren with custom thumbnail support"
-    )
-    AFTER_GET_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nJoin : @LegendBot_AI"
-    AFTER_GET_DL_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nValid for <b>14</b> days.\nJoin : @LegendBot_AI"
-    # AFTER_GET_DL_LINK = " {} valid for 30 or more days.\n\n Join : @LegendBot_AI "
-    AFTER_GET_GOFILE_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n<b>File MD5 Checksum :</b> <code>{}</code>\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\n Valid untill 10 days of inactivity\nJoin : @LegendBot_AI"
-    FF_MPEG_RO_BOT_RE_SURRECT_ED = (
-        """Syntax: /trim HH:MM:SS for screenshot of that specific time."""
-    )
-    FF_MPEG_RO_BOT_STEP_TWO_TO_ONE = "First send /downloadmedia to any media so that it can be downloaded to my local. \nSend /storageinfo to know the media, that is currently downloaded."
-    FF_MPEG_RO_BOT_STOR_AGE_INFO = "Video Duration: {}\nSend /clearffmpegmedia to delete this media, from my storage.\nSend /trim HH:MM:SS [HH:MM:SS] to cu[l]t a small photo / video, from the above media."
-    FF_MPEG_RO_BOT_STOR_AGE_ALREADY_EXISTS = "A saved media already exists. Please send /storageinfo to know the current media details."
+    AFTER_GET_DL_LINK = " <b>File Name :</b> <code>{}</code>\n<b>File Size :</b> {}\n\n<b>⚡Link⚡ :</b> <code>{}</code>\n\nValid for <b>14</b> days.\nJoin : @lakshyajee2pw"
+
     USER_DELETED_FROM_DB = (
         "User <a href='tg://user?id={}'>{}</a> deleted from DataBase."
     )
+
     REPLY_TO_DOC_OR_LINK_FOR_RARX_SRT = (
         "Reply to a Telegram media (MKV), to extract embedded streams"
     )
-    REPLY_TO_MEDIA_ALBUM_TO_GEN_THUMB = (
-        "Reply /generatecustomthumbnail to a media album, to generate custom thumbail"
-    )
-    ERR_ONLY_TWO_MEDIA_IN_ALBUM = "Media Album should contain only two photos. Please re-send the media album, and then try again, or send only two photos in an album."
+
     INVALID_UPLOAD_BOT_URL_FORMAT = "URL format is incorrect. make sure your url starts with either http:// or https://. You can set custom file name using the format link | file_name.extension"
+
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
-    FF_MPEG_RO_BOT_AD_VER_TISE_MENT = "Join : @LegendBot_AI \nThanks to @LegendBoy_XD. "
-    EXTRACT_ZIP_INTRO_ONE = (
-        "Send a compressed file first, Then reply /unzip command to the file."
-    )
-    EXTRACT_ZIP_INTRO_THREE = (
-        "Analyzing received file. ⚠️ This might take some time. Please be patient. "
-    )
-    UNZIP_SUPPORTED_EXTENSIONS = ("zip", "rar")
-    EXTRACT_ZIP_ERRS_OCCURED = "Sorry. Errors occurred while processing compressed file. Please check everything again twice, and if the issue persists, report this to <a href='https://telegram.dog/pw_upload_linkbot'>@LegendBot_AI</a>"
-    EXTRACT_ZIP_STEP_TWO = """Select file_name to upload from the below options.
-You can use /rename command after receiving file to rename it with custom thumbnail support."""
-    CANCEL_STR = "Process Cancelled"
-    ZIP_UPLOADED_STR = "Uploaded {} files in {} seconds"
+    
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
 Free users only 1 request per 30 minutes.
 /upgrade or Try 1800 seconds later."""
-    SLOW_URL_DECED = "Gosh that seems to be a very slow URL. Since you were screwing my home, I am in no mood to download this file. Meanwhile, why don't you try this:==> https://shrtz.me/PtsVnf6 and get me a fast URL so that I can upload to Telegram, without me slowing down for other users."
