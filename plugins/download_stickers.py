449ec61cd4b8e2b615500303c29bf31e041bab94
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-03 16:46:14 +0530

    Update download_stickers.py

--- plugins/download_stickers.py
+++ plugins/download_stickers.py
@@ -31,7 +31,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 @pyrogram.Client.on_message(pyrogram.filters.sticker)
 async def DownloadStickersBot(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
-        await bot.send_messages(
+        await bot.send_message(
             chat_id=update.chat.id,
             text="Buy The Subscriptions From @LegendBoy_XD To Use This Command",
             reply_to_message_id=update.message_id,
