0575cd3aa125e8b9b49eb608651b04384111159d
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-03 16:00:18 +0530

    Update start_command_text.py

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -33,6 +33,6 @@ async def start(client, message):
             [InlineKeyboardButton("Query", url="https://t.me/LegendBot_OP")],
         ]
     )
-    welcomed = f"Hi <b>{message.from_user.first_name}</b>\nThis is Bot Officially made By @LegendBot_XD For Download Pw Video.\n\n♦️ Click Here :- /help To Get How To Use\n⚜ Bot Uptime : {bot_uptime}"
+    welcomed = f"Hi <b>{message.from_user.first_name}</b>\nThis is Bot Advanced Features Bot Officially Made By @LegendBot_XD.\n\n♦️ Click Here :- /help To Get How To Use\n⚜ Bot Uptime : {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
