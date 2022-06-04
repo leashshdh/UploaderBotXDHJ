c5a6ab929969e3233e6b7b045dc0d8d01ca85101
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-04 15:16:33 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -84,8 +84,8 @@ async def start(bot, update):
     bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time))
     joinButton = InlineKeyboardMarkup(
         [
-            [InlineKeyboardButton("JOIN", url="https://t.me/TGBotsCollection")],
-            [InlineKeyboardButton("Try", url="https://t.me/TGBotsCollectionbot")],
+            [InlineKeyboardButton("JOIN", url="https://t.me/LegendBot_AI")],
+            [InlineKeyboardButton("Try", url="https://t.me/LegendBot_OP")],
         ]
     )
     # https://git.io/Jye7k
