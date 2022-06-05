da4c5b56b745441f5efbf22ce76c0bb231485984
leashshdh <97189150+leashshdh@users.noreply.github.com>
2022-06-06 01:21:53 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -24,8 +24,8 @@ class Config(object):
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
     DEF_THUMB_NAIL_VID_S = os.environ.get(
-        "DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90"
-    )
+        "DEF_THUMB_NAIL_VID_S", ""
+    ) # https://placehold.it/90x90
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from
     # https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
