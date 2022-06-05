43b03625bbe112c7c26ae55cf146a30ab8c4772e
X LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2022-06-05 23:16:52 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -385,7 +385,7 @@ async def youtube_dl_call_back(bot, update):
                     height=height,
                     supports_streaming=True,
                     # reply_markup=reply_markup,
-                    thumb=thumb_image_path,
+                    thumb=thumbnail,
                     reply_to_message_id=update.message.reply_to_message.message_id,
                     progress=progress_for_pyrogram,
                     progress_args=(
