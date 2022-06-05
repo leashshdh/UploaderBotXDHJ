dd3c945a59b1d4df57250d0f8f028cfdaf64c35f
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2022-06-05 16:00:53 +0000

    auto

--- plugins/custom_thumbnail.py
+++ plugins/custom_thumbnail.py
@@ -5,11 +5,18 @@
 import os
 import time
 import numpy
+import random
 import logging
 # the logging things
 import pyrogram
 from PIL import Image
+from config import Config
 from translation import Translation
+from pyrogram import Client, filters
+from hachoir.parser import createParser
+from database.database import AddUser, db
+from hachoir.metadata import extractMetadata
+from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
 
 
 logging.basicConfig(
@@ -26,28 +33,15 @@ else:
 
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-import logging
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
-import random
-import numpy
-import os
-from PIL import Image
-import time
 
-from config import Config
 # the Strings used for this "thing"
-from translation import Translation
-from pyrogram import Client, filters
 
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-from database.database import AddUser
-from database.database import db
-from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["setthumb"]))
@@ -63,10 +57,11 @@ async def generate_custom_thumbnail(bot, update):
     thumbnail = await db.get_thumbnail(update.from_user.id)
     if thumbnail is not None:
         await bot.send_photo(
-        chat_id=update.chat.id,
-        photo=thumbnail,
-        caption=f"Your current saved thumbnail 🦠",
-        reply_to_message_id=update.message_id)
+            chat_id=update.chat.id,
+            photo=thumbnail,
+            caption=f"Your current saved thumbnail 🦠",
+            reply_to_message_id=update.message_id,
+        )
     else:
         await update.reply_text(text=f"No Thumbnail found 🤒")
 
@@ -82,8 +77,11 @@ async def save_photo(bot, update):
         return
     await AddUser(bot, update)
     await db.set_thumbnail(update.from_user.id, thumbnail=update.photo.file_id)
-    await bot.send_message(chat_id=update.chat.id, text=Translation.SAVED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
-
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.SAVED_CUSTOM_THUMB_NAIL,
+        reply_to_message_id=update.message_id,
+    )
 
 
 @pyrogram.Client.on_message(pyrogram.filters.command(["delthumb"]))
@@ -91,15 +89,22 @@ async def save_photo(bot, update):
 async def delthumbnail(bot, update):
     await AddUser(bot, update)
     await db.set_thumbnail(update.from_user.id, thumbnail=None)
-    await bot.send_message(chat_id=update.chat.id, text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL, reply_to_message_id=update.message_id)
-
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.DEL_ETED_CUSTOM_THUMB_NAIL,
+        reply_to_message_id=update.message_id,
+    )
 
 
 async def Gthumb01(bot, update):
-    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    thumb_image_path = (
+        Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    )
     db_thumbnail = await db.get_thumbnail(update.from_user.id)
     if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+        thumbnail = await bot.download_media(
+            message=db_thumbnail, file_name=thumb_image_path
+        )
         Image.open(thumbnail).convert("RGB").save(thumbnail)
         img = Image.open(thumbnail)
         img.resize((100, 100))
@@ -109,52 +114,63 @@ async def Gthumb01(bot, update):
 
     return thumbnail
 
+
 async def Gthumb02(bot, update, duration, download_directory):
-    thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    thumb_image_path = (
+        Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+    )
     db_thumbnail = await db.get_thumbnail(update.from_user.id)
     if db_thumbnail is not None:
-        thumbnail = await bot.download_media(message=db_thumbnail, file_name=thumb_image_path)
+        thumbnail = await bot.download_media(
+            message=db_thumbnail, file_name=thumb_image_path
+        )
     else:
-        thumbnail = await take_screen_shot(download_directory, os.path.dirname(download_directory), random.randint(0, duration - 1))
+        thumbnail = await take_screen_shot(
+            download_directory,
+            os.path.dirname(download_directory),
+            random.randint(0, duration - 1),
+        )
 
     return thumbnail
 
+
 async def Mdata01(download_directory):
 
-          width = 0
-          height = 0
-          duration = 0
-          metadata = extractMetadata(createParser(download_directory))
-          if metadata is not None:
-              if metadata.has("duration"):
-                  duration = metadata.get('duration').seconds
-              if metadata.has("width"):
-                  width = metadata.get("width")
-              if metadata.has("height"):
-                  height = metadata.get("height")
+    width = 0
+    height = 0
+    duration = 0
+    metadata = extractMetadata(createParser(download_directory))
+    if metadata is not None:
+        if metadata.has("duration"):
+            duration = metadata.get("duration").seconds
+        if metadata.has("width"):
+            width = metadata.get("width")
+        if metadata.has("height"):
+            height = metadata.get("height")
 
-          return width, height, duration
+    return width, height, duration
+
 
 async def Mdata02(download_directory):
 
-          width = 0
-          duration = 0
-          metadata = extractMetadata(createParser(download_directory))
-          if metadata is not None:
-              if metadata.has("duration"):
-                  duration = metadata.get('duration').seconds
-              if metadata.has("width"):
-                  width = metadata.get("width")
+    width = 0
+    duration = 0
+    metadata = extractMetadata(createParser(download_directory))
+    if metadata is not None:
+        if metadata.has("duration"):
+            duration = metadata.get("duration").seconds
+        if metadata.has("width"):
+            width = metadata.get("width")
 
-          return width, duration
+    return width, duration
+
 
 async def Mdata03(download_directory):
 
-          duration = 0
-          metadata = extractMetadata(createParser(download_directory))
-          if metadata is not None:
-              if metadata.has("duration"):
-                  duration = metadata.get('duration').seconds
+    duration = 0
+    metadata = extractMetadata(createParser(download_directory))
+    if metadata is not None:
+        if metadata.has("duration"):
+            duration = metadata.get("duration").seconds
 
-          return duration
-
+    return duration
